5091c41e8867869a2de6090ad64e52c7cc8eb6a8
github username <personabe1984@gmail.com>
2018-12-28 20:41:07 -0500

    This project will validate length of the string

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    arr:[]
+  }
+
+  deleteAnElement = (index) => {
+    // copy the array
+    const arr = [...this.state.arr];
+    //slice the array
+    arr.splice(index, 1);
+    //then update the state
+    this.setState({arr:arr})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
---
+++ src/ValidationComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+render() {
+  return();
+};
+
+
+export default 
